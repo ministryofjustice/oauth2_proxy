e79128e99e37df8ba0139094414477a7d9dc9a4a
Himal Mandalia <himalmandalia@gmail.com>
2017-04-07 13:56:05 +0100

    Add Ministry of Justice Signon

---
+++ providers/mojsso.go
@@ -0,0 +1,84 @@
+package providers
+
+import (
+	"errors"
+	"fmt"
+	"github.com/bitly/oauth2_proxy/api"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type MoJSSOProvider struct {
+	*ProviderData
+}
+
+func NewMoJSSOProvider(p *ProviderData) *MoJSSOProvider {
+	p.ProviderName = "Ministry of Justice Sign On"
+	if p.LoginURL == nil || p.LoginURL.String() == "" {
+		p.LoginURL = &url.URL{
+			Scheme: "http",
+			Host:   "localhost:3000",
+			Path:   "/oauth/authorize",
+		}
+	}
+	if p.RedeemURL == nil || p.RedeemURL.String() == "" {
+		p.RedeemURL = &url.URL{
+			Scheme: "http",
+			Host:   "localhost:3000",
+			Path:   "/oauth/token",
+		}
+	}
+	// ValidationURL is the API Base URL
+	if p.ProfileURL == nil || p.ProfileURL.String() == "" {
+		p.ProfileURL = &url.URL{
+			Scheme: "http",
+			Host:   "localhost:3000",
+			Path:   "/api/user_details",
+		}
+	}
+	return &MoJSSOProvider{ProviderData: p}
+}
+
+func (p *MoJSSOProvider) GetEmailAddress(s *SessionState) (string, error) {
+	var email string
+	var err error
+
+	if s.AccessToken == "" {
+		return "", errors.New("missing access token")
+	}
+	req, err := http.NewRequest("GET", p.ProfileURL.String(), nil)
+	if err != nil {
+		return "", err
+	}
+
+	header := make(http.Header)
+	header.Set("Authorization", fmt.Sprintf("Bearer %s", s.AccessToken))
+	req.Header = header
+
+	json, err := api.Request(req)
+
+	if err != nil {
+		return "", err
+	}
+
+	email, err = getEmailFromJSON(json)
+
+	if err == nil && email != "" {
+		return email, err
+	}
+
+	email, err = json.Get("email").String()
+
+	if err != nil {
+		log.Printf("failed making request %s", err)
+		return "", err
+	}
+
+	if email == "" {
+		log.Printf("failed to get email address")
+		return "", err
+	}
+
+	return email, err
+}
--- providers/providers.go
+++ providers/providers.go
@@ -18,6 +18,8 @@ type Provider interface {
 
 func New(provider string, p *ProviderData) Provider {
 	switch provider {
+	case "mojsso":
+		return NewMoJSSOProvider(p)
 	case "myusa":
 		return NewMyUsaProvider(p)
 	case "linkedin":
